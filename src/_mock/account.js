9f715df391a0a0cfefc894040ff3afc95575ec9a
tutuCH <tuchenhsien@gmail.com>
2024-07-21 22:02:12 -0400

    init dashboard

---
+++ src/_mock/account.js
@@ -0,0 +1,8 @@
+// ----------------------------------------------------------------------
+
+export const account = {
+  displayName: 'Moldex 3D',
+  email: 'tommytute@gamil.com',
+  // photoURL: '/assets/images/avatars/avatar_25.jpg',
+  photoURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx-9svYKqSjkKCL7wPckPo01DJad9ZFXJc1w&s',
+};
